9261de1619dc1152bada9a6ac259cdcf9f558591
maribelcuales <maribelcuales@gmail.com>
2020-07-03 20:51:18 +0800

    Added map function to iterate over todoItems.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,15 +4,15 @@
 import React from "react";
 import Todo from "./Todo";
 
-const TodoList = (props) => {
+const TodoList = props => {
   return (
     <div>
-      {props.toDoItems.map(item => {
+      {props.todoItems.map(item => (
         <Todo key={item.id} item={item} />
-      })}
+      ))}
       <button>Add Todo</button>
     </div>
-  )
-}
+  );
+};
 
 export default TodoList;
