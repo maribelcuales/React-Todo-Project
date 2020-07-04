2a7f35be65a42ef9b2c252f83f5d3277617eba2f
maribelcuales <maribelcuales@gmail.com>
2020-07-04 15:10:45 +0800

    Added onClick event handler and passed in it the toggledTodo function.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,7 +2,10 @@ import React from "react";
 
 const Todo = props => {
   return (
-    <div>
+    <div
+      onClick={() => props.toggleTodo(props.item.id)}
+      className={`item${props.item.completed ? "completed" : ""}`}
+    >
       <p>{props.item.name}</p>
     </div>
   );
