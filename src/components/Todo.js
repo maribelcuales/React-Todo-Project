e6d9158e1d7f0de140cce3af03585dd44d321f95
maribelcuales <maribelcuales@gmail.com>
2020-07-04 17:07:41 +0800

    Renamed passed in props name to task and added css styling to Todo.css.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,13 @@
 import React from "react";
+import './Todo.css';
 
 const Todo = props => {
   return (
     <div
       onClick={() => props.toggleTodo(props.item.id)}
-      className={`item${props.item.completed ? "completed" : ""}`}
+      className={`item${props.item.completed ? " completed" : ""}`}
     >
-      <p>{props.item.name}</p>
+      <p className="item-todo">{props.item.task}</p>
     </div>
   );
 };
